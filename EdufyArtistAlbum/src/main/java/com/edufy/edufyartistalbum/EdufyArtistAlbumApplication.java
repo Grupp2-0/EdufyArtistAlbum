951d2e52a516d03a3bc0b89d8c1c14059b8733e9
DanielSololidEdu <92855790+DanielSololidEdu@users.noreply.github.com>
2022-10-05 14:19:54 +0200

    Add files via upload

---
+++ EdufyArtistAlbum/src/main/java/com/edufy/edufyartistalbum/EdufyArtistAlbumApplication.java
@@ -0,0 +1,13 @@
+package com.edufy.edufyartistalbum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdufyArtistAlbumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdufyArtistAlbumApplication.class, args);
+    }
+
+}
---
+++ EdufyArtistAlbum/src/test/java/com/edufy/edufyartistalbum/EdufyArtistAlbumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edufy.edufyartistalbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdufyArtistAlbumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
