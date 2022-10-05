bf00639a0c0df4424d50b2a86efa96c9367c63aa
DanielSololidEdu <92855790+DanielSololidEdu@users.noreply.github.com>
2022-10-05 14:24:13 +0200

    Delete src directory

--- src/main/java/com/edufy/edufyartistalbum/repository/ArtistRepo.java
+++
@@ -1,8 +0,0 @@
-package com.edufy.edufyartistalbum.repository;
-
-import com.edufy.edufyartistalbum.model.Artist;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArtistRepo extends JpaRepository<Artist, Integer> {
-    Artist findArtistById(int artistId);
-}
